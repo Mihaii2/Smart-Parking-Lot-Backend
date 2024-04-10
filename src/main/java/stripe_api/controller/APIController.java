ad73126c7d28a67828c6c4f26e2eb9936ce47dc5
simonaAMH <amihaesiisimona5@gmail.com>
2024-04-10 23:02:00 +0300

    .

--- src/main/java/stripe_api/controller/APIController.java
+++ src/main/java/stripe_api/controller/APIController.java
@@ -3,51 +3,41 @@ package stripe_api.controller;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Customer;
-import com.stripe.model.Event;
-import com.stripe.model.PaymentIntent;
-import com.stripe.model.PaymentMethod;
+import com.stripe.model.*;
 import com.stripe.net.Webhook;
 import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.CustomerListParams;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.stripe.param.PaymentMethodAttachParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController //REST returns a ResponseBody object that contains data in a format such as JSON or XML
 public class APIController {
     @Value("${stripe.apiKey}")
     String stripeKey;
 
-    private static int calculateOrderAmount(Object[] items) {
-        //se calculeaza suma totala de plata
+    private static int calculateOrderAmount(String id) {
+        //se calculeaza suma totala de plata gen pretul locului de parcare(dupa id) x nr de ore
         return 1050; //10,50 lei
     }
 
-    static class CreatePaymentItem {
-        @JsonProperty("id")
-        String id;
-        public String getId() {
-            return id;
+    static class PaymentDetails {
+        @JsonProperty("itemID")
+        private String itemID;
+        public String getItemID() {
+            return itemID;
         }
-    }
-
-    static class CreatePayment {
-        @JsonProperty("items")
-        CreatePaymentItem[] items;
-        @JsonProperty("savePaymentMethod")
-        boolean savePaymentMethod;
         @JsonProperty("userID")
-        String userID;
-        public CreatePaymentItem[] getItems() {
-            return items;
-        }
-        public boolean getSavePaymentMethod() {
-            return savePaymentMethod;
-        }
+        private String userID;
         public String getUserID() {
             return userID;
+        }
+        public void setUserID(String id) {
+            this.userID = id;
         }
     }
 
@@ -61,28 +51,54 @@ public class APIController {
         }
     }
 
+    //se cauta in lista de customeri din stripe dupa nume
+    public Customer retrieveByExternalId(String externalId) throws StripeException {
+        CustomerListParams params = CustomerListParams.builder().build();
+        List<Customer> customers = Customer.list(params).getData();
+
+        for (Customer customer : customers) {
+            String customerId = customer.getName(); // Folosește getDescription() în loc de getName()
+
+            if (customerId != null && customerId.equals(externalId)) {
+                return customer; // Returnează clientul găsit în Stripe
+            }
+        }
+
+        return null;
+    }
+
+
     @PostMapping("/create-payment-intent")
-    public CreatePaymentResponse createPaymentIntent(@RequestBody CreatePayment paymentRequest) throws StripeException {
+    public CreatePaymentResponse createPaymentIntent(@RequestBody PaymentDetails paymentRequest) throws StripeException {
         Stripe.apiKey = stripeKey;
 
-        int orderAmount = calculateOrderAmount(paymentRequest.getItems());
-
+        paymentRequest.setUserID("122");
         String customerId = paymentRequest.getUserID(); // ID-ul clientului din aplicatie
-        Customer customer = null;
+        int orderAmount = calculateOrderAmount(paymentRequest.getItemID());
+
+        Customer customer;
+
         try {
-            customer = Customer.retrieve(customerId);//se cauta in Stripe Cuustomers dupa id
-        } catch (StripeException e) { //daca nu exista(cel mai prob  e la prima plata) se creeaza unul nou pt a putea crea PaymentIntent
-            CustomerCreateParams params = CustomerCreateParams.builder()
-                    .setDescription(customerId)
-                    .build();
-            customer = Customer.create(params);
+            if (customerId != null) {
+                customer = retrieveByExternalId(customerId);
+                if (customer == null) {
+                    // Dacă nu există (e la prima plată), se creează unul nou pentru a putea crea PaymentIntent
+                    CustomerCreateParams params = CustomerCreateParams.builder()
+                            .setName(customerId)
+                            .build();
+                    customer = Customer.create(params);
+                }
+            } else {
+                throw new IllegalArgumentException("customerId is null");
+            }
+        } catch (StripeException e) {
+            throw e;
         }
 
         //payment intent ne returneaza ClientSecret - un id secret care ofera informatii despre plata
         PaymentIntentCreateParams paymentParams = PaymentIntentCreateParams.builder()
                 .setAmount((long)orderAmount)
                 .setCustomer(customer.getId())
-                .setSetupFutureUsage(PaymentIntentCreateParams.SetupFutureUsage.OFF_SESSION)
                 .setCurrency("ron")
                 .setAutomaticPaymentMethods(
                         PaymentIntentCreateParams.AutomaticPaymentMethods
@@ -97,4 +113,3 @@ public class APIController {
     }
 
 }
-
